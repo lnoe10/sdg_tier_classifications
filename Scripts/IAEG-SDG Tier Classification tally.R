7c17a02e5f05e5009ed9a89a714d7cdbe6cea47c
Lorenz Noe <lorenznoe@gmail.com>
2021-10-15 12:07:32 -0400

    Fix date

--- Scripts/IAEG-SDG Tier Classification tally.R
+++ Scripts/IAEG-SDG Tier Classification tally.R
@@ -181,4 +181,4 @@ df %>%
   ungroup() %>%
   select(goal, updated_tier, frequency) %>%
   pivot_wider(id_cols = "goal", names_from = "updated_tier", values_from = "frequency") %>%
-  write_csv("Output/Tier Classification frequency 21 Mar 2021.csv", na = ""))
+  write_csv("Output/Tier Classification frequency 29 Mar 2021.csv", na = ""))
